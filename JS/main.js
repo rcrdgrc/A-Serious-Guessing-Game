1a7b06e777a56d64840e0c81a7d36be11eeb371f
rcrdgrc <ricktx04@gmail.com>
2019-05-06 11:32:41 -0500

    added game file

--- JS/main.js
+++ JS/main.js
@@ -4,3 +4,14 @@
 var audio = new Audio(); // define your audio
 
 btn.click( () => audio.play('212579__pepv__evil-laugh.mp3') ); // that will do the trick !!
+
+$("btn").click(
+    function() {
+        console.log("clicked...waiting...");
+
+        setTimeout(
+            function() {
+                alert("Called after delay.");
+            },
+            5000);
+    });
