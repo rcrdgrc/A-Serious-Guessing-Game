0df3df3a88ec7f8379db1e0a029666133ddce33e
rcrdgrc <ricktx04@gmail.com>
2019-05-07 23:05:31 -0500

    added w/l function

--- JS/main.js
+++ JS/main.js
@@ -1,31 +1,28 @@
 
 /*----- constants -----*/ 
-const words = ["ham", "jet", "amber"];
-const wordArray = [];
+const words = ["chainsaw", "dangerous", "demonic", "evil"];
 const wordUArray = [];
 /*----- app's state (variables) -----*/ 
-let lives = 4;
+let wordArray = [];
+let lives = 5;
 let numInWordBank = words.length;
-
 let word = "test";
 let wordU = "";
-
+let used = [];
 /*----- cached element references -----*/ 
+let scary = '<iframe width="460" height="315" src="video/videoplayback.mp4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
 /*----- event listeners -----*/ 
 document.getElementById('letters')
   .addEventListener('click', handleClick)
 
 /*----- functions -----*/
+let pullWord = function() {
+    word = words[(Math.floor(Math.random()*numInWordBank))];
+}
 
 function handleClick(evt) {
     const marker = evt.target.id;
      upDateLetter(marker);
-}
-
-
-
-let pullWord = function() {
-    word = words[(Math.floor(Math.random()*numInWordBank))];
 }
 
 function setUnderline() {
@@ -40,59 +37,51 @@ function setUnderline() {
 }
 
 function upDateLetter(letter) {
-    changes = 0;
     for(i = 0; i < word.length; i++) {
-        wordArray[i] = word.charAt(1);
         if(word.charAt(i) === letter) {
             wordUArray[i] = letter;
-            changes += 1;
-        }
+        } 
     }
-    if(changes < 1) {
+    if(letter !== "letters" && !wordUArray.includes(letter)) {
         lives -= 1;
         document.getElementById("lives").innerHTML = lives;
     }
+    winning();
+}
+
+function winning() {
+
     wordU = wordUArray.join("");
     document.getElementById("WORD").innerHTML = wordU;
     word1 = wordArray.join("");
     word2 = wordUArray.join("");
+
+    console.log(word1);
+    console.log(word2);
     
     if(word1 === word2) {
         alert("You Won!");
         window.location.reload();
     }
-    if(lives < 1) {
-        document.getElementById("WORD").innerHTML === word1;
-        alert("No more lives");
-        window.location.reload();
+   
+    else if(lives === 0) {
+        document.getElementById("video").innerHTML = scary;
+        // alert("No more lives");
+        // window.location.reload();
+        }
     }
-}
 
+// var button = document.querySelector('#letters');
 
+// button.onclick = function () {
+//  var red = Math.floor(Math.random() * 256);
+//  var blue = Math.floor(Math.random() * 256);
+//  var green = Math.floor(Math.random() * 256);
+
+//  this.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+// };
 
 pullWord();
 setUnderline();
 
 
-// clickLetter = function() {
-//     "a".click(function(event){
-//         event.preventDefault();
-//         upDateLetter("a")
-//     });
-// };
-
-
-
-// init();
-
-// function handleClick(evt) {
-//   const picked = evt.target;
-//   const colIdx = parseInt(marker.id.replace('col', ''));
-//   if (isNaN(colIdx) || winner) return;
-//   const rowIdx = board[colIdx].indexOf(0);
-//   if (rowIdx === -1) return;
-//   board[colIdx][rowIdx] = turn;
-//   winner = getWinner();
-//   turn *= -1;
-//   render();
-// }
