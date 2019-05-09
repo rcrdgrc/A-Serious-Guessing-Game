071a7408c0aa2e34732047f774c4dab8d8b624ff
rcrdgrc <ricktx04@gmail.com>
2019-05-09 17:01:42 -0500

    changed the sounds and the keys added sounds to buttons

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,7 @@
 /*----- constants -----*/ 
 const words = ["chainsaw", "dangerous", "demonic", "evil"];
 const wordUArray = [];
+const scarysound = new Audio("assets/sounds/132106__sironboy__woman-scream.wav")
 /*----- app's state (variables) -----*/ 
 let wordArray = [];
 let lives = 5;
@@ -9,39 +10,57 @@ let numInWordBank = words.length;
 let word = "test";
 let wordU = "";
 let used = [];
-/*----- cached element references -----*/ 
-let scary1 = '<iframe width="460" height="315" src="https://i.imgur.com/34aATp9.jpg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-let scary2 = '<iframe width="460" height="315" src="https://i.imgur.com/xyRggKP.png" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-/*----- event listeners -----*/ 
-document.getElementById('letters')
-  .addEventListener('click', handleClick)
 
+/*----- cached element references -----*/ 
+let scary1 = '<iframe width="460 auto" height="315 auto" src="https://i.imgur.com/34aATp9.jpg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+let scary2 = '<iframe width="460 auto" height="315 auto" src="https://i.imgur.com/xyRggKP.png" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+
+/*----- event listeners -----*/ 
+document.getElementById('letters').addEventListener('click', handleClick)
+document.getElementById('reset').addEventListener('click', reload)
+document.getElementById('reset2').addEventListener('click', reload)
 /*----- functions -----*/
+
+
+function reload() {
+    window.location.reload();
+}
+function hideButton() { 
+    document.getElementById('reset').style.visibility = "hidden";
+    document.getElementById('reset2').style.visibility = "hidden";
+    document.getElementById('letters').style.visibility = "visible";
+}
+function showButton() {
+    document.getElementById('reset').style.visibility = "visible";
+    document.getElementById('letters').style.visibility = "hidden";
+}
+function showbutton2() {
+    document.getElementById('reset2').style.visibility = "visible";
+    document.getElementById('letters').style.visibility = "hidden";
+}
 let pullWord = function() {
     word = words[(Math.floor(Math.random()*numInWordBank))];
 }
-
 function setUnderline() {
     pullWord();
-    for(i=0; i<word.length; i++) {
-        wordArray[i] = word.charAt(i);
-        wordUArray[i] = "_";
+    for(index=0; index<word.length; index++) {
+        wordArray[index] = word.charAt(index);
+        wordUArray[index] = "_";
     }
     wordU = wordUArray.join("");
     document.getElementById("WORD").innerHTML = wordU;
     document.getElementById("numLetters").innerHTML = word.length;
 }
-
 function handleClick(evt) {
     const marker = evt.target.id;
      upDateLetter(marker);
 }
 
-
 function upDateLetter(letter) {
-    for(i = 0; i < word.length; i++) {
-        if(word.charAt(i) === letter) {
-            wordUArray[i] = letter;
+    for(index = 0; index < word.length; index++) {
+        if(word.charAt(index) === letter) {
+            wordUArray[index] = letter;
         } 
     }
     if(letter !== "letters" && !wordUArray.includes(letter)) {
@@ -50,7 +69,6 @@ function upDateLetter(letter) {
     }
     winning();
 }
-
 function winning() {
 
     wordU = wordUArray.join("");
@@ -63,17 +81,21 @@ function winning() {
     
     if(word1 === word2) {
         document.getElementById("video").innerHTML = scary2;
+        scarysound.play();
+        showButton();
         // window.location.reload();
     }
    
     else if(lives === 0) {
-      document.getElementById("video").innerHTML = scary1;
+      document.getElementById("video").innerHTML = scary2;
+      scarysound.play();
+      showbutton2();
         }
     }
 
-
-
 pullWord();
 setUnderline();
+hideButton();
+
 
 
